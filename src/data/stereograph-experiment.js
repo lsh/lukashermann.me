58b8613dc903ef13f565a6153a7f8b258fe7fc84
Lukas Hermann <lsh@users.noreply.github.com>
2019-01-06 23:16:46 -0500

    Begin structure of next shader to upload

---
+++ src/data/stereograph-experiment.js
@@ -0,0 +1,10 @@
+export default {
+  id: 13,
+  name: 'Stereograph Experiment',
+  date: 'Fall 2018',
+  type: 'shader',
+  thumbnail: "stereograph-experiment.png",
+  slug: "stereograph-experiment",
+  frag:
+  ``
+}
